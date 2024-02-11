2142ad6162af18e73d5c71b38154756c81b7709b
Ivan Drenjanin <drenjanin.i@gmail.com>
2024-02-11 17:12:20 +0100

    add: tailwind forms; add: pages to templ components; Added tailwindcss form plugin and adjusted the folder structure within the templ components to contain the pages sub folder that will hold the pages that are rendered to the user

--- api/handlers/public_home.go
+++ api/handlers/public_home.go
@@ -5,10 +5,10 @@ import (
 
 	"github.com/a-h/templ"
 
-	"github.com/ivandrenjanin/go-chat-app/components"
+	"github.com/ivandrenjanin/go-chat-app/components/pages"
 )
 
 func PublicHomeHandler() http.HandlerFunc {
 	// we do the thing
-	return templ.Handler(components.Hello("There!")).ServeHTTP
+	return templ.Handler(pages.Hello("There!")).ServeHTTP
 }
