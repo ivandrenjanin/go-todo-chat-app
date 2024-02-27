83d54c165314ddf3d33ea37424904066e3fedf16
Ivan Drenjanin <drenjanin.i@gmail.com>
2024-02-27 19:00:55 +0100

    fix: create project handler funcs

---
+++ api/api_handlers/project_handler.go
@@ -0,0 +1,98 @@
+package apihandlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+
+	"github.com/ivandrenjanin/go-chat-app/app"
+	"github.com/ivandrenjanin/go-chat-app/views/components"
+)
+
+func DeleteProjectHandler(ps *app.ProjectService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		strid := chi.URLParam(r, "projectId")
+		id, err := strconv.Atoi(strid)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		u := r.Context().Value("user").(app.User)
+		err = ps.RemoveProject(r.Context(), u, id)
+		if err != nil {
+			msg := err.Error()
+			if msg == "Forbidden Operation" {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+			} else {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func renderProjectTableComponent(w http.ResponseWriter, r *http.Request, ps *app.ProjectService) {
+	u := r.Context().Value("user").(app.User)
+	pc, err := ps.FindProjectsByUserId(r.Context(), u.ID)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	headers := []string{"Project Name", "Description", "Actions"}
+	rows := make([][]string, 0, cap(pc))
+	base := "/api/projects"
+
+	for _, project := range pc {
+		subBase := fmt.Sprintf("%s/%d", base, project.ID)
+		assign := fmt.Sprintf("%s/assign", subBase)
+		r := []string{assign, subBase, project.Name, project.Description}
+		rows = append(rows, r)
+	}
+
+	c := templ.Handler(components.ProjectTable(headers, rows))
+	c.ServeHTTP(w, r)
+}
+
+func GetProjectByUserIdHandler(ps *app.ProjectService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		renderProjectTableComponent(w, r, ps)
+	}
+}
+
+func CreateProjectHandler(ps *app.ProjectService) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		type requestBody struct {
+			Name        string `validate:"required,min=2,max=32"`
+			Description string `validate:"required,min=2,max=32"`
+		}
+
+		r.ParseForm()
+		var rb requestBody
+		rb.Name = r.Form.Get("name")
+		rb.Description = r.Form.Get("description")
+		if err := validator.New().Struct(rb); err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		u := r.Context().Value("user").(app.User)
+		_, err := ps.CreateProject(r.Context(), u, rb.Name, rb.Description)
+		if err != nil {
+			// TODO: Handle this properly
+			http.Error(
+				w,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError,
+			)
+		}
+
+		renderProjectTableComponent(w, r, ps)
+	}
+}
--- api/routes.go
+++ api/routes.go
@@ -1,23 +1,18 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 
-	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
-	"github.com/go-playground/validator/v10"
 
 	ah "github.com/ivandrenjanin/go-chat-app/api/api_handlers"
 	ch "github.com/ivandrenjanin/go-chat-app/api/component_handlers"
 	ph "github.com/ivandrenjanin/go-chat-app/api/page_handlers"
 	"github.com/ivandrenjanin/go-chat-app/app"
-	"github.com/ivandrenjanin/go-chat-app/views/components"
 )
 
 func addRoutes(
@@ -66,94 +61,15 @@ func addRoutes(
 		r.Post("/login", ah.LoginHandler(is))
 	})
 
-	mux.Route("/api/users", func(r chi.Router) {
+	mux.Route("/api/projects", func(r chi.Router) {
+		// Protected Routes
+		r.Use(MakeIdentityMiddleware(is, us))
+		r.Get("/", ah.GetProjectByUserIdHandler(ps))
+		r.Delete("/{projectId}", ah.DeleteProjectHandler(ps))
+		r.Post("/", ah.CreateProjectHandler(ps))
 	})
 
-	mux.Route("/api/projects", func(r chi.Router) {
-		r.Use(MakeIdentityMiddleware(is, us))
-		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-			u := r.Context().Value("user").(app.User)
-			pc, _ := ps.FindProjectsByUserId(r.Context(), u.ID)
-
-			headers := []string{"Project Name", "Description", "Actions"}
-			rows := make([][]string, 0, cap(pc))
-			base := "/api/projects"
-
-			for _, project := range pc {
-				subBase := fmt.Sprintf("%s/%d", base, project.ID)
-				assign := fmt.Sprintf("%s/assign", subBase)
-				r := []string{assign, subBase, project.Name, project.Description}
-				rows = append(rows, r)
-			}
-
-			c := templ.Handler(components.ProjectTable(headers, rows))
-			c.ServeHTTP(w, r)
-		})
-
-		r.Delete("/{projectId}", func(w http.ResponseWriter, r *http.Request) {
-			strid := chi.URLParam(r, "projectId")
-			id, err := strconv.Atoi(strid)
-			if err != nil {
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-
-			u := r.Context().Value("user").(app.User)
-			err = ps.RemoveProject(r.Context(), u, id)
-			if err != nil {
-				msg := err.Error()
-				if msg == "Forbidden Operation" {
-					http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-				} else {
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				}
-				return
-			}
-			w.WriteHeader(http.StatusOK)
-		})
-
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			type requestBody struct {
-				Name        string `validate:"required,min=2,max=32"`
-				Description string `validate:"required,min=2,max=32"`
-			}
-
-			r.ParseForm()
-			var rb requestBody
-			rb.Name = r.Form.Get("name")
-			rb.Description = r.Form.Get("description")
-			if err := validator.New().Struct(rb); err != nil {
-				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-				return
-			}
-
-			u := r.Context().Value("user").(app.User)
-			_, err := ps.CreateProject(r.Context(), u, rb.Name, rb.Description)
-			if err != nil {
-				// TODO: Handle this properly
-				http.Error(
-					w,
-					http.StatusText(http.StatusInternalServerError),
-					http.StatusInternalServerError,
-				)
-			}
-
-			pc, _ := ps.FindProjectsByUserId(r.Context(), u.ID)
-
-			headers := []string{"Project Name", "Description", "Actions"}
-			rows := make([][]string, 0, cap(pc))
-			base := "/api/projects"
-
-			for _, project := range pc {
-				subBase := fmt.Sprintf("%s/%d", base, project.ID)
-				assign := fmt.Sprintf("%s/assign", subBase)
-				r := []string{assign, subBase, project.Name, project.Description}
-				rows = append(rows, r)
-			}
-
-			c := templ.Handler(components.ProjectTable(headers, rows))
-			c.ServeHTTP(w, r)
-		})
+	mux.Route("/api/users", func(r chi.Router) {
 	})
 
 	mux.Route("/api/todo", func(r chi.Router) {
